028937d8964c1745b61c09a886f63c3708d27aae
Andre Lucas <andrelucastic@gmail.com>
2024-09-19 15:53:42 +0100

    prepare services

---
+++ web-app-server/src/main/resources/static/scripts/solaceauth.js
@@ -0,0 +1,37 @@
+/**
+ * This function POSTs to a dummy authentication service fronted by PubSub+.
+ * The result of the POST is translated to a boolean and provided back to the caller
+ * via the callback function.
+ *
+ * @param {string} sUsername - Username to use for autentication
+ * @param {string} sPassword - Password associated with the username
+ *
+ * @param {oResultCallback} oResultCallback - callback function to execute on success or failure events.
+ */
+function authenticate(sUsername, sPassword, oResultCallback) {
+    'use strict';
+
+    // Enter serverUrl
+    var serverUrl = 'http://localhost:8081/solace/cloud/proxy',
+    jsonBody = {
+        username: sUsername,
+        password: sPassword
+    };
+
+    // Complete fetch parameters; method and headers
+    fetch(serverUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(jsonBody)        
+    })
+        .then(function (response) {
+            console.debug(response);
+            if (response.ok) {
+                oResultCallback(true);
+            } else {
+                oResultCallback(false);
+            }
+        });
+}
---
+++ web-app-server/src/test/java/io/andrelucas/webappserver/WebAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.andrelucas.webappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

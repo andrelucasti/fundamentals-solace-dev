d599e6e467e3ee863611cbf29c6a5420c0c69de1
Andre Lucas <andrelucastic@gmail.com>
2024-09-19 21:10:09 +0100

    Building App - Sending the REST Request

---
+++ web-app-server/src/main/java/io/andrelucas/webappserver/SolaceCloudProxy.java
@@ -0,0 +1,60 @@
+package io.andrelucas.webappserver;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Controller
+public class SolaceCloudProxy {
+
+    public record SolaceUserLogin(String username, String password) { }
+
+    private final String host;
+    private final String username;
+    private final String password;
+
+    public SolaceCloudProxy(@Value("${solace.host}") String host,
+                            @Value("${solace.username}") String username,
+                            @Value("${solace.password}") String password) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+    }
+
+    private HttpHeaders httpHeaders;
+
+    @PostConstruct
+    public void init(){
+        httpHeaders = new HttpHeaders() {{
+            var auth = username + ":" + password;
+            var encode = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.US_ASCII));
+            var authHeader = "Basic " + new String(encode);
+            set("Authorization", authHeader);
+            set("Content-Type", "application/json");
+        }};
+    }
+
+
+
+    @PostMapping("/solace/cloud/proxy")
+    @ResponseBody
+    public ResponseEntity<Void> login(@RequestBody SolaceUserLogin user) {
+        var request = new HttpEntity<>(user, httpHeaders);
+
+        var restTemplate = new RestTemplate();
+        restTemplate.postForObject(host + "/LOGIN/MSG/REQUEST", request, String.class);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
